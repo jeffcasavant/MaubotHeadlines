6d35d6321a4bc17563e0b88f3b8deb0c153b2bfa
Jeff Casavant <jeff.casavant@gmail.com>
2023-02-11 17:24:18 -0500

    Initial

---
+++ Makefile
@@ -0,0 +1,25 @@
+FILES=headlines.py scripts/build_maubot_yaml.py
+
+security:
+	poetry run safety check
+	poetry run bandit $(FILES)
+
+lint:
+	poetry run pylint $(FILES)
+	poetry run black --check $(FILES)
+	poetry run isort --check $(FILES)
+
+format:
+	poetry run black $(FILES)
+	poetry run isort $(FILES)
+
+build: maubot.yaml
+	poetry run mbc build
+
+requirements.txt: poetry.lock
+	poetry export -f requirements.txt --without-hashes | sed 's/;.*//g' > requirements.txt
+
+maubot.yaml: pyproject.toml requirements.txt
+	poetry run scripts/build_maubot_yaml.py
+
+test: lint security
---
+++ headlines.py
@@ -0,0 +1,43 @@
+from typing import Type
+
+from maubot import MessageEvent, Plugin
+from maubot.handlers import command
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+
+
+class Config(BaseProxyConfig):
+    def do_update(self, helper: ConfigUpdateHelper) -> None:
+        helper.copy("newsapi_api_key")
+
+async def lookup(http, api_key, headline):
+    params = "&".join(f"{k}={v}" for k, v in {"q": headline, "sortBy": "popularity", "apiKey": api_key}.items())
+    url = f"https://newsapi.org/v2/everything?{params}"
+    print(url)
+
+    async with http.get(url) as response:
+        response = await response.json()
+
+        if response["totalResults"] == 0:
+            return None
+
+        return response["articles"][0]["url"]
+
+class HeadlinesPlugin(Plugin):
+    async def start(self) -> None:
+        await super().start()
+        self.config.load_and_update()
+
+    @classmethod
+    def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
+
+    @command.new("findheadline")
+    @command.argument("headline", pass_raw=True, required=True)
+    async def handler(self, evt: MessageEvent, headline: str) -> None:
+        self.log.debug("Looking up \"%s\"", headline)
+        url = lookup(self.http, self.config["newsapi_api_key"], headline)
+
+        if url:
+            await evt.reply(url)
+        else:
+            await evt.reply("Could not find headline")
